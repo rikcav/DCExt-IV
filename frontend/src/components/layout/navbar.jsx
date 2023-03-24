91b2215f24448c7ea6dc3cabb80ca2b39bb0a85d
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-03-24 02:33:10 -0300

    Home

--- frontend/src/components/layout/navbar.jsx
+++ frontend/src/components/layout/navbar.jsx
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
-import './style.css'
-export function Navbar(){
-    
-        return (
-            <div className="navbar" style={{ margin: 0 }}>
-                
-                <p>DCExT-iv</p>
-                
-            </div>
-        );
-    };
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+  return (
+    <div>
+      <div className="navbar">
+        <p>DCExT-iv</p>
+      </div>
+      <p className='body'>Lorem ipsum dolor sit amet consectetur. Mauris posuere morbi condimentum purus donec pharetra. In tincidunt metus lacinia venenatis risus egestas. Duis tincidunt sit pharetra ac egestas. Nibh lectus sit sit gravida. Netus ultrices maecenas quam morbi est fusce.
+Accumsan eget consequat tempus augue. Egestas eu etiam sit at. At pharetra orci lorem volutpat quis integer adipiscing. Tincidunt cursus placerat mus in porttitor. At vitae leo viverra cras pellentesque. Mi vitae at rhoncus adipiscing laoreet feugiat. Elit lacinia blandit molestie ut posuere. Blandit ultrices pharetra ullamcorper mattis. Placerat tellus arcu ac integer donec feugiat enim. Hendrerit elit nunc tellus felis tincidunt a platea molestie aliquam. Ut gravida enim senectus urna felis. Tempor ultrices condimentum cras eget libero. Pellentesque morbi tincidunt odio donec faucibus. Ultrices hendrerit nisl eu sapien amet bibendum euismod quis tellus.
+         <div class="circle-row">
+         <div class="circle red"></div>
+        <div class="circle blue"></div>
+        <div class="circle blue"></div>
+        <div class="circle red"></div>
+        </div>
+      </p>
+      <footer className="footer">
+        <img
+          src="https://cdn.discordapp.com/attachments/440326168491720705/1088685664796680202/image.png"
+          alt="Login App"
+        />
+      </footer>
+    </div>
+  );
+}
 
 export default Navbar;
