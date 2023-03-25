5c382ba34e94c3324aaec260f9c71a34189e5b14
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-03-24 21:40:15 -0300

    att

--- frontend/src/components/layout/navbar.jsx
+++ frontend/src/components/layout/navbar.jsx
@@ -10,11 +10,11 @@ function Navbar() {
       <p className='body'>Lorem ipsum dolor sit amet consectetur. Mauris posuere morbi condimentum purus donec pharetra. In tincidunt metus lacinia venenatis risus egestas. Duis tincidunt sit pharetra ac egestas. Nibh lectus sit sit gravida. Netus ultrices maecenas quam morbi est fusce.
 Accumsan eget consequat tempus augue. Egestas eu etiam sit at. At pharetra orci lorem volutpat quis integer adipiscing. Tincidunt cursus placerat mus in porttitor. At vitae leo viverra cras pellentesque. Mi vitae at rhoncus adipiscing laoreet feugiat. Elit lacinia blandit molestie ut posuere. Blandit ultrices pharetra ullamcorper mattis. Placerat tellus arcu ac integer donec feugiat enim. Hendrerit elit nunc tellus felis tincidunt a platea molestie aliquam. Ut gravida enim senectus urna felis. Tempor ultrices condimentum cras eget libero. Pellentesque morbi tincidunt odio donec faucibus. Ultrices hendrerit nisl eu sapien amet bibendum euismod quis tellus.
 <div class="circle-row">
-          <a href="/deficienciafisica" className='circle-container'>
+          <a href="/pcd" className='circle-container'>
             <div class="circle red">
               <img src='https://cdn.discordapp.com/attachments/440326168491720705/1088971888388231270/image.png' alt="Deficiência Física"/>
             </div>
-            <p class="description">Deficiência Física</p>
+            <p class="description">PCD</p>
           </a>
 
           <a href="/idosos" className='circle-container'>
