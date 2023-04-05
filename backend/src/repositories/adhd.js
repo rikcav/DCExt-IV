7844b6410b1e59c195fccd06e19be1593b6fff31
rikcav <henriquearau000@gmail.com>
2023-04-05 08:21:34 -0300

    feat: todos os repositórios

---
+++ backend/src/repositories/adhd.js
@@ -0,0 +1,18 @@
+import Prisma, { prisma } from "../services/prisma.js";
+
+exports.createAdhd = async (data) => {
+  const adhd = await prisma.adhd.create(data);
+  return adhd;
+};
+
+exports.getById = async (id) => {
+  const adhd = await prisma.adhd.findUnique({
+    where: id,
+  });
+  return adhd;
+};
+
+exports.getAll = async () => {
+  const allAdhd = await prisma.adhd.findMany();
+  return allAdhd;
+};
