5c51996cf34d06a973173ec8dd89b1a3eed17b5f
rikcav <henriquearau000@gmail.com>
2023-04-11 15:14:11 -0300

    validations

---
+++ backend/src/validations/autism.js
@@ -0,0 +1,8 @@
+import * as yup from "yup";
+
+export const autismValidation = yup.object({
+  type: yup.string().required(),
+  name: yup.string().required,
+  desciption: yup.string().required(),
+  category: yup.string().required,
+});
