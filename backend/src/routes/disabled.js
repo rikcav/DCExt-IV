afdb5fdc95b73f592a8bab44338a92582317e8f8
David Rocha <manoeudavi3@gmail.com>
2023-04-12 20:41:31 -0300

    adicionado rotas do disabled

--- backend/src/routes/disabled.js
+++ backend/src/routes/disabled.js
@@ -1,6 +1,11 @@
-import { create } from "../controllers/disabled";
+import { create, get, getId } from "../controllers/disabled";
 const disabled = (app) => {
+  //create
   app.post("/disabled/create", create);
+  //getAll
+  app.get("/disabled/get", get)
+  //getById
+  app.get("/disabled/get/:id", getId)
 };
 
 export default disabled;
