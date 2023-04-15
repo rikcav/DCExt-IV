d388f9cc4af6dd5d73111e70c10f1aa8b6e89e7a
rikcav <henriquearau000@gmail.com>
2023-04-15 11:40:18 -0300

    feat: updated autism controller

--- backend/src/controllers/autism.js
+++ backend/src/controllers/autism.js
@@ -1,17 +1,33 @@
-import req from "express/lib/request"
-import { createAutism, getAll, getById} from "../repositories/autism"
-import {autismValidation} from "../validations/autism"
+import req from "express/lib/request";
+import { createAutism, getAll, getById } from "../repositories/autism";
+import { autismValidation } from "../validations/autism";
 
-export const create = async(req,res) =>{
-    try {
+export const create = async (req, res) => {
+  try {
+    //conferir se todos os campos foram preenchidos
+    // await autismValidation.validate(req.body)
 
-        //conferir se todos os campos foram preenchidos
-       // await autismValidation.validate(req.body)
+    const autism = await createAutism(req.body);
+    res.status(200).send(autism);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
 
-        const autism = await createAutism(req.body)
-        res.status(200).send(autism)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-}
+export const get = async (req, res) => {
+  try {
+    const autism = await getAll();
+    res.status(200).send(autism);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
 
+export const getId = async (req, res) => {
+  try {
+    const autism = await getById(Number(req.params.id));
+    res.status(200).send(autism);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
