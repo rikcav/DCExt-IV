d51ff585e693060511782467aad5e03d5fd7f03f
rikcav <henriquearau000@gmail.com>
2023-04-15 11:43:17 -0300

    feat: updated autism routes

--- backend/src/routes/autism.js
+++ backend/src/routes/autism.js
@@ -1,6 +1,11 @@
-import { create } from "../controllers/autism";
+import { create, get, getId } from "../controllers/autism";
 const autism = (app) => {
+  //create
   app.post("/autism/create", create);
+  //getAll
+  app.get("/autism/get", get)
+  //getById
+  app.get("/autism/get/:id", getId)
 };
 
 export default autism;
