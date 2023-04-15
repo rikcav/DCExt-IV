47634872fba214283deddac2379b7aa1994bfe5d
rikcav <henriquearau000@gmail.com>
2023-04-15 11:40:32 -0300

    feat: updated elder controller

--- backend/src/controllers/elder.js
+++ backend/src/controllers/elder.js
@@ -1,16 +1,33 @@
-import req from "express/lib/request"
-import { createElder, getAll, getById} from "../repositories/elder"
-import {elderValidation} from "../validations/elder"
+import req from "express/lib/request";
+import { createElder, getAll, getById } from "../repositories/elder";
+import { elderValidation } from "../validations/elder";
 
-export const create = async(req,res) =>{
-    try {
+export const create = async (req, res) => {
+  try {
+    //conferir se todos os campos foram preenchidos
+    //await elderValidation.validate(req.body)
 
-        //conferir se todos os campos foram preenchidos
-        //await elderValidation.validate(req.body)
+    const elder = await createElder(req.body);
+    res.status(200).send(elder);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
 
-        const elder = await createElder(req.body)
-        res.status(200).send(elder)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-}
+export const get = async (req, res) => {
+  try {
+    const elder = await getAll();
+    res.status(200).send(elder);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+export const getId = async (req, res) => {
+  try {
+    const elder = await getById(Number(req.params.id));
+    res.status(200).send(elder);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
