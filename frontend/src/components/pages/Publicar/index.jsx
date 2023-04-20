cd94f46c3e5b79355e91c832ad5d40f1bdfa1b95
ElisonMartins <martinselison10@gmail.com>
2023-04-20 16:27:38 -0300

    feat: Pagina publicar criada com a rota

---
+++ frontend/src/components/pages/Publicar/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navbar from '../../layout/navbar';
+import Footer from '../../layout/Footer';
+import './style.css';
+
+function Publicar() {
+    return (
+      <>
+        <Navbar/>
+        <div>
+          oubklxcn lk
+        </div>
+        <Footer/>
+          
+      </>
+    );
+  }
+  
+  export default Publicar;
