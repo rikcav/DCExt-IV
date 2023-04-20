a38188063e5e11fbcdf4569cfb4abb1acad3aa11
ElisonMartins <martinselison10@gmail.com>
2023-04-20 16:13:39 -0300

    fix: bug do footer sobrepondopagina home concertado

--- frontend/src/components/pages/home/index.jsx
+++ frontend/src/components/pages/home/index.jsx
@@ -10,9 +10,9 @@ function Home() {
         <Navbar />
         <p className='body' >Lorem ipsum dolor sit amet consectetur. Mauris posuere morbi condimentum purus donec pharetra. In tincidunt metus lacinia venenatis risus egestas. Duis tincidunt sit pharetra ac egestas. Nibh lectus sit sit gravida. Netus ultrices maecenas quam morbi est fusce.N porttitor. At vitae leo viverra cras pellentesque. Mi vitae at rhoncus adipiscing laoreet feugiat. Elit lacinia blandit molestie ut posuere. Blandit ultrices pharetra ullamcorper mattis. Placerat tellus arcu ac integer donec feugiat enim. Hendrerit elit nunc tellus felis tincidunt a platea molestie aliquam. Ut gravida enim senectus urna felis. Tempor ultrices condimentum cras eget libero. Pellentesque morbi tincidunt odio donec faucibus. Ultrices hendrerit nisl eu sapien amet bibendum euismod quis tellus.</p>
         <CircleRow />
-        
-        </body>
         <Footer />
+      </body>
+        
         </>
     );
   }
