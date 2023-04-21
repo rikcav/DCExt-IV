7bb1f4d4f9863b48baa5e757a73b0f657111548e
ElisonMartins <martinselison10@gmail.com>
2023-04-20 22:30:58 -0300

    feat: adicionado opçoes em categorias

--- frontend/src/components/pages/Publicar/index.jsx
+++ frontend/src/components/pages/Publicar/index.jsx
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import { useState } from "react";
-import Navbar from '../../layout/navbar';
-import Footer from '../../layout/Footer';
-import './style.css';
+import React, { useState } from "react";
+import Navbar from "../../layout/navbar";
+import Footer from "../../layout/Footer";
+import "./style.css";
 
 function Publicar() {
   const [formData, setFormData] = useState({
     name: "",
     type: "",
     description: "",
-    category: "",
+    category: [],
     link: "",
     selected: "",
   });
 
+  const categories = ["Comunicação", "Foco", "Habilidades cognitivas", "Pensamento lógico", "Aprendizagem", "Imaginação", "Coordenação motora"];
+
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
+    if (name === "category") {
+      const options = e.target.options;
+      const selectedCategories = [];
+      for (let i = 0; i < options.length; i++) {
+        if (options[i].selected) {
+          selectedCategories.push(options[i].value);
+        }
+      }
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        category: selectedCategories,
+      }));
+    } else {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: value,
+      }));
+    }
   };
 
   const handleSubmit = (e) => {
@@ -29,17 +44,31 @@ function Publicar() {
       name: "",
       type: "",
       description: "",
-      category: "",
+      category: [],
       link: "",
       selected: "",
     });
   };
 
-    return (
-      <div>
-        <Navbar/>
-       <div className='formulario'>
-        <form className="form" onSubmit={handleSubmit}>
+  const handleCategoryChange = (e) => {
+    const options = e.target.options;
+    const values = [];
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        values.push(options[i].value);
+      }
+    }
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      category: values,
+    }));
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="formulario">
+      <form className="form" onSubmit={handleSubmit}>
       <div>
         <label htmlFor="name">Nome:</label>
         <input
@@ -74,14 +103,22 @@ function Publicar() {
       </div>
       <div>
         <label htmlFor="category">Categoria:</label>
-        <input
-          type="text"
+        <select
           id="category"
           name="category"
           value={formData.category}
           onChange={handleChange}
           required
-        />
+        >
+          <option value="">Selecione uma opção</option>
+          <option value="comunicacao">Comunicação</option>
+          <option value="foco">Foco</option>
+          <option value="habilidades_cognitivas">Habilidades cognitivas</option>
+          <option value="pensamento_logico">Pensamento lógico</option>
+          <option value="aprendizagem">Aprendizagem</option>
+          <option value="imaginacao">Imaginação</option>
+          <option value="coordenacao_motora">Coordenação motora</option>
+        </select>
       </div>
       <div>
         <label htmlFor="link">Link:</label>
@@ -114,11 +151,10 @@ function Publicar() {
         <button type="submit">Publicar</button>
       </div>
     </form>
+      </div>
+      <Footer />
     </div>
-        <Footer/>
-        
-        </div>
-    );
-  }
-  
-  export default Publicar;
+  );
+}
+
+export default Publicar;
